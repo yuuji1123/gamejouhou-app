9b955de7be160d56ed4f324e34e345ae4668a81b
yuuji1123 <wagnas.syoukankyou@gmail.com>
2023-01-11 15:25:35 +0900

    コメント機能の追加

--- app/controllers/communications_controller.rb
+++ app/controllers/communications_controller.rb
@@ -1,11 +1,11 @@
 class CommunicationsController < ApplicationController
   def create
     communication = Communication.create(cammunication_params)
-    redirect_to "/game/#{communication.game.id}"
+    redirect_to "/games/#{communication.game.id}"
   end
 
   private
   def cammunication_params
-    params.require(:cammunication).permit(:text).merge(user_id: current_user.id, game_id: params[:game_id])
+    params.require(:communication).permit(:text).merge(user_id: current_user.id, game_id: params[:game_id])
   end
 end
